fc2f8ac924155886924d9e506331e24ae58a8f70
NTPLSRPP <nathaphat.n07@gmail.com>
2023-12-18 19:44:02 +0700

    add

---
+++ src/main/java/int202/prefinalint202/filters/AuthenticationFilter.java
@@ -0,0 +1,26 @@
+package int202.prefinalint202.filters;
+
+import int202.prefinalint202.entities.Customer;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "AuthenticationFilter", servletNames = {"OfficeServlet"})
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpSession session = ((HttpServletRequest) request).getSession();
+        Customer currentUser = (Customer) session.getAttribute("currentUser");
+        if (currentUser == null) {
+            session.setAttribute("error", "please login first");
+            ((HttpServletResponse) response).sendRedirect("../005/login");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+}
---
+++ src/main/java/int202/prefinalint202/servlets/LoginServlet.java
@@ -0,0 +1,53 @@
+package int202.prefinalint202.servlets;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import int202.prefinalint202.entities.Customer;
+import int202.prefinalint202.repositories.CustomerRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/020/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action") == null ? "" : request.getParameter("action");
+        if (action.equalsIgnoreCase("logout")) {
+            HttpSession session = request.getSession(false);
+            session.setAttribute("currentUser", null);
+            response.sendRedirect("../");
+        } else {
+            request.getRequestDispatcher("../login.jsp").forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+        HttpSession session = request.getSession();
+        CustomerRepository customerRepository = new CustomerRepository();
+        Customer currentUser = customerRepository.findByUserName(userName);
+        if (currentUser == null) {
+            request.setAttribute("error", "wrong username");
+            session.setAttribute("error", null);
+            doGet(request, response);
+        } else {
+            Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2d, 16, 16);
+            char[] passwordArray = password.toCharArray();
+            boolean isCorrect = argon2.verify(currentUser.getPassword(), passwordArray);
+            if (isCorrect) {
+                session.setAttribute("currentUser", currentUser);
+                session.setAttribute("error", null);
+                response.sendRedirect("../");
+            } else {
+                request.setAttribute("error", "wrong password");
+                session.setAttribute("error", null);
+                doGet(request, response);
+            }
+        }
+    }
+}
